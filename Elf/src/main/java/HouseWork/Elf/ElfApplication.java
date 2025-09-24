ecfcc9d094860f8a16d55114c4db5a41f4f5ca35
JLin056 <jlin890516@gmail.com>
2025-09-24 16:07:13 +0800

    git add init

---
+++ Elf/src/main/java/HouseWork/Elf/ElfApplication.java
@@ -0,0 +1,13 @@
+package HouseWork.Elf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElfApplication.class, args);
+	}
+
+}
---
+++ Elf/src/test/java/HouseWork/Elf/ElfApplicationTests.java
@@ -0,0 +1,13 @@
+package HouseWork.Elf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
